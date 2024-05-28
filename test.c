18e937a91bf490cb87f0a86fae09c0f320482d9f
Venkey HM <156779372+Venky0426@users.noreply.github.com>
2024-05-28 08:49:47 +0530

    Update test.c

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
+    sum = number1 + number222222;     
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
